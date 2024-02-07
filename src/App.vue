b20fed286bf5b0d7ccdbac732655f7ad875360ae
Torstein A. Bø <torsteinibo@gmail.com>
2024-02-07 21:59:35 +0100

    MVP

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,114 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, watch } from 'vue'
+import TopBar from '@tibnor/openbridge-webcomponents-vue/components/top-bar/ObcTopBar'
+import '@tibnor/openbridge-webcomponents/dist/components/navigation-item/navigation-item.js'
+import BrillianceMenu from '@tibnor/openbridge-webcomponents-vue/components/brilliance-menu/ObcBrillianceMenu'
+import ObcAlertTopbarElement from '@tibnor/openbridge-webcomponents-vue/components/alert-topbar-element/ObcAlertTopbarElement'
+
+import '@tibnor/openbridge-webcomponents/dist/icons/icon-14-alarm-unack'
+
+import { AlertType } from '@tibnor/openbridge-webcomponents/dist/types'
+
+if (import.meta.env.PROD) {
+  //@ts-expect-error TS2306
+  import('@tibnor/openbridge-webcomponents/dist/icons/index.js')
+}
+
+const date = ref(new Date().toISOString())
+
+onMounted(() => {
+  // update date every second
+  setInterval(() => {
+    date.value = new Date().toISOString()
+  }, 1000)
+
+  //@ts-expect-error TS2306
+  import('@tibnor/openbridge-webcomponents/dist/icons/index.js')
+})
+
+const palette = ref('day')
+
+watch(
+  palette,
+  (value) => {
+    // set data-obc-theme attribute on html element
+    document.documentElement.setAttribute('data-obc-theme', value)
+  },
+  { immediate: true }
+)
+
+function onPaletteChange(event: CustomEvent) {
+  palette.value = event.detail.value;
+}
+
+const showBrilliance = ref(false)
+
+function toggleBrilliance() {
+  showBrilliance.value = !showBrilliance.value
+}
 </script>
 
+<!-- eslint-disable vue/no-deprecated-slot-attribute -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <TopBar
+      app-title="Demo"
+      page-name="Vue"
+      :date="date"
+      @dimming-button-clicked="toggleBrilliance"
+      show-apps-button
+      show-dimming-button
+      show-clock
+    >
+      <template #alerts>
+        <ObcAlertTopbarElement
+          style="width: 500px"
+          :n-alerts="0"
+          :max-width="500"
+          :alert-type="AlertType.None"
+          :show-ack="false"
+          :alert-muted="true"
+        >
+        </ObcAlertTopbarElement>
+      </template>
+    </TopBar>
+  </header>
+  <main>
+    <div class="content">
+      <BrillianceMenu
+        :palette="palette"
+        @palette-changed="onPaletteChange"
+        show-auto-brightness
+        class="brilliance"
+        v-if="showBrilliance"
+      >
+      </BrillianceMenu>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+header {
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  right: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.content {
+  isolation: isolate;
+  position: absolute;
+  top: 48px;
+  bottom: 0;
+  left: 0;
+  right: 0;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+  .brilliance {
+    position: absolute;
+    top: 4px;
+    right: 48px;
+  }
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import '@tibnor/openbridge-webcomponents/src/palettes/variables.css'
 createApp(App).mount('#app')
